8c07ca909cb3d5756d347eafbaf05fd86bdbfe0d
MarceloASG <marcelo_augusto.soares@hotmail.com>
2022-11-08 16:39:06 -0300

    criacao dos packages base

---
+++ src/main/java/com/mycompany/gestaofuncionario/model/Funcionario.java
@@ -0,0 +1,5 @@
+package com.mycompany.gestaofuncionario.model;
+
+public class Funcionario {
+    
+}
---
+++ src/main/java/com/mycompany/gestaofuncionario/presenter/PrincipalPresenter.java
@@ -0,0 +1,5 @@
+package com.mycompany.gestaofuncionario.presenter;
+
+public class PrincipalPresenter {
+
+}
